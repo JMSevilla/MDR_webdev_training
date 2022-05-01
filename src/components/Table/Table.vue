ced543bf7639ea5775332c735cf02c1bea180e0b
unknown <miggysvll@gmail.com>
2022-05-01 21:01:23 +0800

    day 2 source update

---
+++ src/components/Table/Table.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <table class="table table-outline table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Firstname</th>
+      <th scope="col">Lastname</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="item in List" :key="item.id">
+      <th scope="row">{{item.id}}</th>
+      <td>{{item.firstname}}</td>
+      <td>{{item.lastname}}</td>
+      <td>
+          <b-button variant="primary" size="small" @click="Editable(item.id)">Edit</b-button> &nbsp;
+          <b-button variant="danger" size="small" @click="Deletable(item.id)">Delete</b-button>
+      </td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+</template>
+
+<script>
+export default {
+    props : {
+        List : Array,
+        Editable : Function,
+        Deletable : Function
+    }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <Navbar />
+    <div style="margin-top: 100px" class="container">
+      <div class="card card-body">
+        <h3>Users List</h3>
+        <Table 
+          :List="listonArray"
+          :Editable="handleEdit"
+          :Deletable="handleDelete"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar/Navbar'
+import Table from '@/components/Table/Table'
+export default {
+  components: {
+    Navbar, Table
+  },
+  data(){
+    return{
+      listonArray : [{id: 1, firstname: "bryan", lastname: "palad"}]
+    }
+  },
+  methods : {
+    handleEdit: function(id) {
+      alert(id)
+    },
+    handleDelete: function(id) {
+      alert(id)
+    }
+  }
+}
+</script>
